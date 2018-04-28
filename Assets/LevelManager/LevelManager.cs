5027285fdfc805ad93b4610cea3abf4becb8a334
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2018-04-27 22:54:05 -0300

    Added ValentinesMatchThree Project

---
+++ Assets/LevelManager/LevelManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+  [SerializeField] int scoreNeeded = 15000;
+  [SerializeField] GameObject successModal;
+  [SerializeField] GameObject failModal;
+  [SerializeField] GameObject canvas;
+  
+
+  public void OnTimeUp()
+  {
+		float score = Score.GetScore();
+    if (score >= scoreNeeded) {
+			LevelSuccess();
+		} else {
+			LevelFail();
+		}
+  }
+
+  void LevelSuccess()
+  {
+    Vector3 middleOfScreen = new Vector3(Screen.width / 2f, Screen.height / 2f , 0f);
+    GameObject modal = Instantiate(successModal, middleOfScreen, Quaternion.identity);
+    modal.transform.parent = canvas.transform;
+  }
+
+  void LevelFail()
+  {
+    Vector3 middleOfScreen = new Vector3(Screen.width / 2f, Screen.height / 2f , 0f);
+    GameObject modal = Instantiate(successModal, middleOfScreen, Quaternion.identity);
+    modal.transform.parent = canvas.transform;
+  }
+
+
+}
---
+++ Assets/Timer/Timer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+  [SerializeField] float totalTime = 60;
+  [SerializeField] float timeLeft = 60;
+  [SerializeField] Color startTimeColor = Color.green;
+  [SerializeField] Color endTimeColor = Color.red;
+
+  Slider slider;
+  GameObject sliderFill;
+  Image sliderFillimage;
+  LevelManager levelManager;
+
+
+  // Use this for initialization
+  void Start()
+  {
+    slider = GetComponentInChildren<Slider>();
+
+    sliderFill = slider.transform.Find("Fill Area").Find("Fill").gameObject;
+    sliderFillimage = sliderFill.GetComponent<Image>();
+    sliderFillimage.color = startTimeColor;
+
+    levelManager = GameObject.FindObjectOfType<LevelManager>();
+
+    StartCoroutine("Countdown");
+  }
+
+  IEnumerator Countdown()
+  {
+    while (true)
+    {
+      yield return new WaitForSeconds(1);
+      timeLeft -= 1f;
+      slider.value = timeLeft / totalTime;
+      sliderFillimage.color = Color.Lerp(endTimeColor, startTimeColor, slider.value);
+      if (timeLeft < 0f)
+      {
+        StopCoroutine("Countdown");
+        Destroy(sliderFill);
+        levelManager.OnTimeUp();
+      }
+    }
+  }
+}
