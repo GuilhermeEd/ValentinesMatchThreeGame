7cac03cdffd848bd6d5892662c9ec4920f026783
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2018-06-08 21:10:02 -0300

    Added Heart Background

---
+++ Assets/Heart Background/HeartBackground.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartBackground : MonoBehaviour {
+
+	[SerializeField] Sprite[] frames;
+
+	public int framesPerSecond = 30;
+	Image image;
+	// Use this for initialization
+	void Start () {
+		image = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int i = (int) (Time.time * framesPerSecond) % frames.Length;
+		image.sprite = frames[i];
+	}
+}
